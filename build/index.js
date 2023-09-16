f80c4d4cab0496eef45d13fe1b04d8d0fdf37e24
unknown <raimundolourival@gmail.com>
2023-09-15 22:31:47 -0300

    apis e expres

--- build/index.js
+++ build/index.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 console.log('O aplicativo foi iniciado');
-console.log(database_1.tusers);
-console.log(database_1.tprodutos);
+(0, database_1.createUser)(3, 'gleice', 'gleice@gmail.com', '45689');
+(0, database_1.getAllUsers)();
+(0, database_1.createProduct)(3, 'camisa', 20, 'só uma camisa', 'asdfvbbbbbb');
+(0, database_1.getProducts)();
+console.table((0, database_1.searchProductsByName)('caixa'));
 //# sourceMappingURL=index.js.map
